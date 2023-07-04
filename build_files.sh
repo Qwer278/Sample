c2f75b569e02f22f7e546adefd7b1846120acd5a
Ayush Jindal(Jindalayush987@gmal.com) <94276821+Qwer278@users.noreply.github.com>
2023-07-04 14:57:01 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo "BUILD START"
 python3.9 -m pip install django
-python3.9 -m pip install pysqlite
+python3.9 -m pip install pysqlite3
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
